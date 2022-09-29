ef680aac5ce011cc133855e55cee913fdb6e7d2e
ShourovHasan <shourovhasan@gmail.com>
2022-09-29 13:41:26 +0600

    time calculation added

--- src/components/Fitness/Fitness.js
+++ src/components/Fitness/Fitness.js
@@ -30,7 +30,59 @@ const FitnessCards = () => {
                     
                 </div>
             </div>
-            
+            <div className='fitness_Calculation'>
+                <div className='profile'>
+                    <img src='profile.jpg' alt="" />
+                    <div className='profile_details'>
+                        <p><strong>Shourov Hasan</strong> <br /> <span><FontAwesomeIcon icon={faLocationDot}></FontAwesomeIcon> Dhaka, Bangladesh</span></p>
+                        
+                    </div>
+                </div>
+                <div className='my_fitness'>
+                    <div>
+                        <span><strong>95</strong>kg</span><br />
+                        <span>Weight</span>
+                    </div>
+                    <div>
+                        <span><strong>6.1</strong></span><br />
+                        <span>Height</span>
+                    </div>
+                    <div>
+                        <span><strong>26</strong>yrs</span><br />
+                        <span>Age</span>
+                    </div>
+                </div>
+                <h2>Add A Break</h2>
+                <div className='break_time'>
+                    <div>
+                        <p><span>10</span>s</p>
+                    </div>
+                    <div>
+                        <p><span>20</span>s</p>
+                    </div>
+                    <div>
+                        <p><span>30</span>s</p>
+                    </div>
+                    <div>
+                        <p><span>40</span>s</p>
+                    </div>
+                    <div>
+                        <p><span>50</span>s</p>
+                    </div>
+                </div>
+                <h2>Exercise Details</h2>
+                <div className='exercise_time'>
+                    <p>Exercise time</p>
+                    <p><strong>0</strong> seconds</p>
+                </div>
+                <div className='break_times'>
+                    <p>Exercise time</p>
+                    <p><strong>0</strong> seconds</p>
+                </div>
+                <div className='completed_btn'>
+                    <button>Activity Completed</button>
+                </div>
+            </div>
         </div>
     );
 };
